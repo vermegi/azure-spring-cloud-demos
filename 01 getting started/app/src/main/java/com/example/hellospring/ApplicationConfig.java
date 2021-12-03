a1af19fc1414510dc1e4969cc3ced2315f6eaf4f
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2021-12-03 13:40:55 +0000

    added pre-destroy

---
+++ 01 getting started/app/src/main/java/com/example/hellospring/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.example.hellospring;
+
+import org.springframework.context.annotation.Configuration;
+import javax.annotation.PreDestroy;
+
+@Configuration
+public class ApplicationConfig {
+
+ @PreDestroy
+ public void onShutDown() {
+  System.out.println("closing application context..let's do the final resource cleanup");
+ }
+}
